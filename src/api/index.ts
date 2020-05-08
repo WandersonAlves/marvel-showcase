cae533153fe5647e670123a55d2f6ca1fe63a36b
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-05-07 23:30:51 -0300

    feat: add axios provider

---
+++ src/api/index.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'https://gateway.marvel.com:443',
+});
+
+API.interceptors.request.use(request => {
+  request.params = request.params ? request.params : {};
+  request.params.apikey = '8fdee667c960c83cbe427ba898d269f5';
+  return request;
+});
+
+export default API;
