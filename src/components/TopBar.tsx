15d0f6fb9654c2bd349e0009575d3e891394db19
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2020-05-10 02:01:19 -0300

    fix(design): remove width from topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 const TopBar = styled(FlexRow)`
   background-color: ${MarvelRedColor};
   padding: 40px;
-  width: 100%;
   align-items: center;
   justify-content: center;
 `;
